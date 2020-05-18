a4e58ee1d51015e4dfc33c538bc0b5cc7c5194fd
SanujaTharinda <sanujatharinda@gmail.com>
2020-05-19 00:34:51 +0530

    Initial Implementation Complete

---
+++ index.php
@@ -0,0 +1,43 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>User Login</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+
+</head>
+<body>
+	<div class="login">
+		<img src="img/avatar.png" class="avatar">
+		<form method="post" action="connection.php">
+
+				<p>
+					<h1>LOG IN</h1>
+				</p>
+
+				<p>
+					<label for="">Username:</label>
+					<input type="text" name="email" id="email" placeholder="Email Address">
+				</p>
+
+				<p>
+					<label for="">Password:</label>
+					<input type="password" name="password" id="password" placeholder="Password">
+				</p>
+
+				<p>
+					<button type="submit" name="submit">Login</button>
+				</p>
+
+
+
+
+		</form>
+
+	</div>
+
+</body>
+</html>
